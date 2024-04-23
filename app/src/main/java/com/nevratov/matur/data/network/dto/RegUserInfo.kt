e91c0dc54ed72fff3ff5c198ba2015bfd2be006a
admin <nevratov22@gmail.com>
2024-04-23 10:04:55 +0300

    Register screen - step 1 (name, last name) created

---
+++ app/src/main/java/com/nevratov/matur/data/network/dto/RegUserInfo.kt
@@ -0,0 +1,8 @@
+package com.nevratov.matur.data.network.dto
+
+data class RegUserInfo(
+    val name: String,
+    val gender: String,
+    val email: String,
+    val birthday: String
+)
---
+++ app/src/main/java/com/nevratov/matur/presentation/main/RegisterScreen.kt
@@ -0,0 +1,97 @@
+package com.nevratov.matur.presentation.main
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nevratov.matur.R
+import com.nevratov.matur.data.network.RetrofitInstance
+import com.nevratov.matur.data.network.dto.RegUserInfo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Preview
+@Composable
+fun RegisterScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_m),
+            contentDescription = stringResource(R.string.logo_matur_description)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(text = "Создать аккаунт", fontSize = 26.sp)
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(text = "Введите своё имя", fontSize = 16.sp)
+        Spacer(modifier = Modifier.height(22.dp))
+        FirstNameTextField()
+        Spacer(modifier = Modifier.height(8.dp))
+        LastNameTextField()
+        Spacer(modifier = Modifier.height(32.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+            Button(onClick = { }) {
+                Text(text = stringResource(R.string.next_label))
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstNameTextField() {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = { text = it },
+        label = { Text(text = stringResource(R.string.name_label)) }
+    )
+}
+
+@Composable
+fun LastNameTextField() {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = { text = it },
+        label = { Text(text = stringResource(R.string.family_label)) }
+    )
+}
+
+
+
+
+
+
+
