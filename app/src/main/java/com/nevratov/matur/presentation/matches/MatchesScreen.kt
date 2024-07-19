e6326cd757feaa75ae89f32f423e68ac60982d01
admin <nevratov22@gmail.com>
2024-07-19 16:09:08 +0300

    MatchesScreen and ProfileScreen UI updated (topBar)

--- app/src/main/java/com/nevratov/matur/presentation/matches/MatchesScreen.kt
+++ app/src/main/java/com/nevratov/matur/presentation/matches/MatchesScreen.kt
@@ -14,8 +14,12 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,6 +35,7 @@ import com.nevratov.matur.R
 import com.nevratov.matur.domain.entity.User
 import com.nevratov.matur.navigation.NavigationState
 import com.nevratov.matur.presentation.BottomNavigationBar
+import com.nevratov.matur.ui.theme.MaturAlternativeColor
 
 @Composable
 fun MatchesScreen(
@@ -73,6 +78,7 @@ fun MatchesScreen(
     }
 
     Scaffold(
+        topBar = { TopBar() },
         bottomBar = { BottomNavigationBar(navigationState = navigationState) }
     ) { paddingValues ->
         Box(
@@ -91,28 +97,20 @@ private fun MatchesContent(
     items: List<User>,
     onMatchUserClicked: (User) -> Unit
 ) {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Text(
-            text = stringResource(R.string.Matches_label),
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        LazyVerticalGrid(
-            modifier = Modifier.fillMaxSize(),
-            columns = GridCells.Fixed(2),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+    LazyVerticalGrid(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        columns = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
 
-        ) {
-            items(items = items, key = { it.id }) {
-                MatchesItem(
-                    user = it,
-                    onItemClicked = onMatchUserClicked
-                )
-            }
+    ) {
+        items(items = items, key = { it.id }) {
+            MatchesItem(
+                user = it,
+                onItemClicked = onMatchUserClicked
+            )
         }
     }
 }
@@ -146,5 +144,22 @@ private fun MatchesItem(
             fontSize = 18.sp
         )
     }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TopBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.Matches_label),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaturAlternativeColor,
+            titleContentColor = MaterialTheme.colorScheme.background
+        )
+    )
 }
--- app/src/main/java/com/nevratov/matur/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/nevratov/matur/presentation/profile/ProfileScreen.kt
@@ -6,19 +6,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.nevratov.matur.R
 import com.nevratov.matur.navigation.NavigationState
 import com.nevratov.matur.presentation.BottomNavigationBar
+import com.nevratov.matur.ui.theme.MaturAlternativeColor
 
 @Composable
 fun ProfileScreen(
     navigationState: NavigationState
 ) {
     Scaffold(
+        topBar = { TopBar() },
         bottomBar = { BottomNavigationBar(navigationState = navigationState) }
     )  { paddingValues ->
         Box(
@@ -36,4 +46,22 @@ fun ProfileScreen(
         }
     }
 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TopBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.profile_navigation_item),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaturAlternativeColor,
+            titleContentColor = MaterialTheme.colorScheme.background
+        )
+    )
 }
