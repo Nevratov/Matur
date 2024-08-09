c85bab6de52146e2278a3b79b3836ad34ca73ce0
admin <nevratov22@gmail.com>
2024-08-09 21:17:56 +0300

    handling lost connection for LoginScreen

--- app/src/main/java/com/nevratov/matur/presentation/main/login/LoginViewModel.kt
+++ app/src/main/java/com/nevratov/matur/presentation/main/login/LoginViewModel.kt
@@ -7,7 +7,10 @@ import androidx.lifecycle.viewModelScope
 import com.nevratov.matur.R
 import com.nevratov.matur.domain.entity.LoginData
 import com.nevratov.matur.domain.usecases.LoginUseCase
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,20 +22,35 @@ class LoginViewModel @Inject constructor(
     private val _screenState = MutableStateFlow<LoginScreenState>(LoginScreenState.initialState)
     val screenState = _screenState
 
+    private var lastSuccessfulState = LoginScreenState.initialState
+    private var toastConnectionLost: Toast? = null
+    private val exceptionHandler = CoroutineExceptionHandler { _, _ ->
+        toastConnectionLost?.cancel()
+        toastConnectionLost = Toast.makeText(
+            application,
+            application.getString(R.string.connection_lost_toast),
+            Toast.LENGTH_SHORT
+        )
+        toastConnectionLost?.show()
+        _screenState.value = lastSuccessfulState
+    }
+
+
     fun login(email: String, password: String) {
         val currentState = screenState.value
         if (currentState !is LoginScreenState.Content) return
+        lastSuccessfulState = currentState.copy(email = email, password)
 
         _screenState.value = LoginScreenState.Loading
         val loginData = LoginData(email = email, password = password)
-        viewModelScope.launch {
+        viewModelScope.launch(context = exceptionHandler) {
             val answer = loginUseCase(loginData)
             if (!answer) Toast.makeText(
                 application,
                 application.getString(R.string.wrong_login_data_toast),
                 Toast.LENGTH_LONG
             ).show()
-            _screenState.value = currentState.copy(email = email, password = password)
+            _screenState.value = lastSuccessfulState
         }
     }
 }
